252d40fb3355f77b4b535bea8d4a50fcea032f56
Bryan Giger <bryan@gigerbyt.es>
2019-08-30 17:57:14 +0200

    added templates and some example views

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # import dependencies
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 # bootstrap the app
 app = Flask(__name__)
@@ -11,8 +11,25 @@ port = int(os.getenv('PORT', '3000'))
 # our base route which just returns a string
 @app.route('/')
 def hello_world():
-    return 'Congratulations! Welcome to the Swisscom Application Cloud.'
+    return 'Congrats, you made your first website!!!'
+
+@app.route('/index')
+def with_string():
+    html = """
+        <h1> This is a paragraph </h1>
+        <li>list</li>
+        """
+    return html
+
+@app.route('/withtemplate')
+def other_view():
+    return render_template("test.html")
+
+@app.route('/better')
+def with_css():
+    my_name = "Marcelo"
+    return render_template("withcss.html", name=my_name)
 
 # start the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
